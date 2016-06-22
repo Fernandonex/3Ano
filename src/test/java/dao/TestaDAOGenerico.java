20b39a4af4c7036ec5cc6c754fbeb3b0961e8348
Fernando <fernando_latrel@hotmail.com>
2016-06-21 22:50:37 -0300

    Ajustes no teste de deletar.

--- src/test/java/dao/TestaDAOGenerico.java
+++ src/test/java/dao/TestaDAOGenerico.java
@@ -28,20 +28,27 @@ public class TestaDAOGenerico {
 		System.out.println("Denuncia 1 "+denuncia1.getEndereco() );
 		denuncia1.setEndereco("Rua Z, 1221");
 		dao.salvar(denuncia1);
-		System.out.println("Denuncia 1 "+denuncia1.getEndereco() ); 
-		Assert.assertEquals("Rua Z, 1221", denuncia1.getEndereco());
+		System.out.println("Denuncia 1 "+denuncia1.getEndereco()); 
+		MausTratos objetoRec1 = (MausTratos) dao.recupera(MausTratos.class, 1L);
+		Assert.assertEquals("Rua Z, 1221", objetoRec1.getEndereco());
 		/*FIM ALTERAR*/
 		
 		/*INICIO DELETAR*/
-		Long id3 = denuncia3.getId();
-		System.out.println("O Id da denuncia 3 é: "+id3);
-		dao.exluir(denuncia3);
-		//Teste 1
 		List<MausTratos> denuncias1 = dao.lista(MausTratos.class);
-		Assert.assertEquals(2, denuncias1.size());
+		System.out.println(denuncias1.size());
+		
+		MausTratos objetoRec2 = (MausTratos) dao.recupera(MausTratos.class, 1L);
+		dao.exluir(objetoRec2);
+		//Teste 1
+		List<MausTratos> denuncias2 = dao.lista(MausTratos.class);
+		Assert.assertEquals(2, denuncias2.size());
 		//Teste 2
-		MausTratos objetoRec = (MausTratos) dao.recupera(MausTratos.class, id3);
+		MausTratos objetoRec = (MausTratos) dao.recupera(MausTratos.class, 1L);
 		Assert.assertEquals(null, objetoRec);
+		
+		List<MausTratos> denuncias3 = dao.lista(MausTratos.class);
+		System.out.println(denuncias3.size());
 		/*FIM DELETAR*/
+		
 	}
 }
